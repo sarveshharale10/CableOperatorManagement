c1acdc817217471f36aeb0e8d78a8ad902b5f6cc
sarvesh <sarvesh@gmail.com>
2018-10-30 09:25:46 +0530

    Frame Title Changing

--- CableOperator.java
+++ CableOperator.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import javax.swing.event.*;
 
-class ContainerFrame extends JFrame{
+class ContainerFrame extends JFrame implements ChangeListener{
 	JTabbedPane tabbedPane;
 	JPanel customerPanel,collectionPanel,complaintPanel;
 	ContainerFrame(){
-		super("Cable Operator Management");
+		super("Cable Operator Management - Customer");
 		for(UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
 			if ("Windows".equals(info.getName())) {
 				try{
@@ -25,14 +26,35 @@ class ContainerFrame extends JFrame{
 		tabbedPane.add("Collection",collectionPanel);
 		tabbedPane.add("Complaints",complaintPanel);
 
+		tabbedPane.addChangeListener(this);
+
 		add(tabbedPane);
 
 		pack();
 		//setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
 
+	public void stateChanged(ChangeEvent e){
+		JTabbedPane pane = (JTabbedPane) e.getSource();
+		int sel = pane.getSelectedIndex();
+				
+		switch(sel){
+			case 0:
+			setTitle("Cable Operator Management - Customer");
+			break;
 
+			case 1:
+			setTitle("Cable Operator Management - Collection");
+			break;
+
+			case 2:
+			setTitle("Cable Operator Management - Complaints");
+			break;
+
+		}
+				
 	}
 }
 
